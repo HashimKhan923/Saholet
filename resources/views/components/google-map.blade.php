03902e4f1ec7fbf8962f1137cb3180fca9edd82d
Muhammad Awais <awais.dev2serve@gmail.com>
2026-07-29 00:56:50 +0500

    aspect ratio of maps fixed with 500px height

--- resources/views/components/google-map.blade.php
+++ resources/views/components/google-map.blade.php
@@ -10,19 +10,17 @@
         </div>
     @endif
 
-    <div class="overflow-hidden rounded-2xl border border-slate-200 bg-white shadow-sm dark:border-slate-800 dark:bg-slate-900">
-        <div class="aspect-square w-full">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5000.6383632312!2d67.13077336423349!3d24.962569279684093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb34b7ea3c7553b%3A0x7deaeb9437cf9ae3!2sSahoulat!5e1!3m2!1sen!2s!4v1785177357745!5m2!1sen!2s"
-                width="100%"
-                height="100%"
-                style="border:0;"
-                allowfullscreen=""
-                loading="lazy"
-                referrerpolicy="strict-origin-when-cross-origin"
-                title="Sahoulat office location on Google Maps"
-                class="block h-full w-full"
-            ></iframe>
-        </div>
-    </div>
+    
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5000.6383632312!2d67.13077336423349!3d24.962569279684093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb34b7ea3c7553b%3A0x7deaeb9437cf9ae3!2sSahoulat!5e1!3m2!1sen!2s!4v1785177357745!5m2!1sen!2s"
+            width="100%"
+            height="500px"
+            style="border:0;"
+            allowfullscreen
+            loading="lazy"
+            referrerpolicy="strict-origin-when-cross-origin"
+            title="Sahoulat office location on Google Maps"
+            class="block rounded-2xl"
+        ></iframe>
+    
 </div>
